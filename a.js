5df74219df38508ebe83c71f32b6407b9053b080
hao <a@gmail.com>
2022-09-28 22:33:59 +0800

    test

---
+++ a.js
@@ -0,0 +1,10 @@
+let movie = {
+    title:"刻在你心底的名字",
+    actors:[{
+        name:"陳浩森",
+        age:24
+    }]
+}
+// document.write(movie.title) //刻在你心底的名字
+// document.write(movie.actors[0].name)//陳浩森
+document.write(movie.actors[0].age); //24
